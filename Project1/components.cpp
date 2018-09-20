5dea1e3aa97abb5de7bd49d4a89336beae6d6391
HardyWillia <hardywillia@outlook.com>
2018-09-19 23:33:53 -0400

    Initial commit and file creation

---
+++ Project1/components.cpp
@@ -0,0 +1,16 @@
+/*This file:
+    - Declares an adjacency list
+    - Prompts the user for input file to graph data
+    - Returns an iterator
+    - Builds the adjacency list
+    - Displays the list
+    - Compares vector components
+    - Merges lists together if they have at least one element in common
+    - Removes the empty list after it has been merged
+    */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+
