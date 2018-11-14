68ed1f4326f74f262bba658606a20cc46378f160
Lia Hardy <hardywillia@outlook.com>
2018-11-13 21:58:25 -0500

    Update mergesort.h

--- Project3/mergesort.h
+++ Project3/mergesort.h
@@ -1,20 +1,72 @@
 #ifndef mergesort_h
 #define mergesort_h
-#include <vector>
-using std::vector;
 
-class MergeSort
+/**
+ * Internal method that merges two sorted halves of a subarray.
+ * a is an array of Comparable items.
+ * tmpArray is an array to place the merged result.
+ * leftPos is the left-most index of the subarray.
+ * rightPos is the index of the start of the second half.
+ * rightEnd is the right-most index of the subarray.
+ */
+template <typename Comparable>
+void merge( vector<Comparable> & a, vector<Comparable> & tmpArray,
+int leftPos, int rightPos, int rightEnd )
 {
-public:
-  MergeSort(vector<int> a);
-  void sort();
-  vector<int> &getVec() { return vec; }
+    int leftEnd = rightPos - 1;
+    int tmpPos = leftPos;
+    int numElements = rightEnd - leftPos + 1;
 
-private:
-  vector<int> vec;
-  void mergeSort(vector<int> &a);
-  void mergeSort(vector<int> &a, vector<int> &tmpArray, int left, int right);
-  void merge(vector<int> &a, vector<int> &tmpArray, int leftPos, int rightPos, int rightEnd);
-};
+    // Main loop
+    while( leftPos <= leftEnd && rightPos <= rightEnd )
+        if( a[ leftPos ] <= a[ rightPos ] )
+            tmpArray[ tmpPos++ ] = std::move( a[ leftPos++ ] );
+        else
+            tmpArray[ tmpPos++ ] = std::move( a[ rightPos++ ] );
 
-#endif // mergesort_h
+    while( leftPos <= leftEnd ) // Copy rest of first half
+        tmpArray[ tmpPos++ ] = std::move( a[ leftPos++ ] );
+
+    while( rightPos <= rightEnd ) // Copy rest of right half
+        tmpArray[ tmpPos++ ] = std::move( a[ rightPos++ ] );
+
+    // Copy tmpArray back
+    for( int i = 0; i < numElements; ++i, --rightEnd )
+        a[ rightEnd ] = std::move( tmpArray[ rightEnd ] );
+}
+
+
+/**
+ * Internal method that makes recursive calls.
+ * a is an array of Comparable items.
+ * tmpArray is an array to place the merged result.
+ * left is the left-most index of the subarray.
+ * right is the right-most index of the subarray.
+ */
+
+template <typename Comparable>
+void mergeSort( vector<Comparable> & a, vector<Comparable> & tmpArray, int left, int right )
+{
+    if( left < right )
+    {
+        int center = (left + right) / 2;
+        mergeSort( a, tmpArray, left, center );
+        mergeSort( a, tmpArray, center + 1, right );
+        merge( a, tmpArray, left, center + 1, right );
+    }
+}
+
+template <typename Comparable>
+void mergeSort( vector<Comparable> & a )
+{
+    
+    vector<Comparable> tmpArray( a.size( ));
+    
+    mergeSort( a, tmpArray, 0, a.size( ) -1 );
+}
+
+
+
+
+
+#endif /* mergesort_h */
